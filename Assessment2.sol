aac4c210958d86de0048f73b1b110c26550c4a8d
Abhinav Deokuliar <113229623+AbhinavDeokuliar@users.noreply.github.com>
2024-06-09 01:40:13 +0530

    Add files via upload

---
+++ Assessment2.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.26;
+
+contract Token {
+
+    
+    string public Token_Name;
+    string public Token_Abbre;
+    uint public Total_Supply;
+
+    mapping (address => uint) public Balance;
+    
+    function Mint (address address_, uint value) public {
+        Balance[address_] += value;
+        Total_Supply += value;
+    }
+
+    function Burn (address address_, uint value) public {
+        require (Balance[address_] >= value, "Insufficient Balance");
+        Balance[address_] -= value;
+        Total_Supply -= value;
+    }
+
+}
+
